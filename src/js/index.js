f455ed40a0085a679a26636afa4d420b4bc7707e
Leonardo Morini <leo.morini.nn@gmail.com>
2025-04-12 16:22:38 -0300

    Gerando os personagens via JS

--- src/js/index.js
+++ src/js/index.js
@@ -1,40 +1,139 @@
-/*
-  O que precisamos fazer? 
-  Quando clicar no botão do personagem na lista, temos que:
-  1. Marcar o botão como selecionado
-  2. Mostrar o personagem correspondente
-*/
+// Array com todos os personagens
+const charactersDBZ = [
+  {
+    id: "cyberstorm",
+    title: "Cyberstorm",
+    description: `Kael Takeda, conhecido no submundo digital como "Cyberstorm", é um programador prodígio especializado em segurança cibernética e inteligência artificial. Com um intelecto afiado e reflexos rápidos, ele domina linguagens de programação como se fossem extensões de sua própria mente.`,
+    imgDesktop: "./src/img/bg-cyberstorm.png",
+    imgMobile: "./src/img/bg-cyberstorm-mobile.png",
+    imgIcon: "./src/img/icone-cyberstorm.png",
+  },
+  {
+    id: "codepixie",
+    title: "CodePixie",
+    description: `Lía Nakamura, conhecida como "CodePixie", é uma programadora genial que domina o desenvolvimento de algoritmos e inteligência artificial. Com um talento nato para decifrar padrões, ela cria sistemas inovadores e protege dados sensíveis de ameaças digitais.`,
+    imgDesktop: "./src/img/bg-codepixie.png",
+    imgMobile: "./src/img/bg-codepixie-mobile.png",
+    imgIcon: "./src/img/icone-codepixie.png",
+  },
+  {
+    id: "hexblade",
+    title: "Hexblade",
+    description: `Ryo Tanaka, conhecido como "HexBlade", é um hacker habilidoso que opera nas sombras do ciberespaço. Mestre em engenharia reversa e criptografia, ele combina força bruta e inteligência afiada para quebrar firewalls impenetráveis.`,
+    imgDesktop: "./src/img/bg-hexblade.png",
+    imgMobile: "./src/img/bg-hexblade-mobile.png",
+    imgIcon: "./src/img/icone-hexblade.png",
+  },
+  {
+    id: "neonpulse",
+    title: "Neonpulse",
+    description: `NeonPulse Ayla Vega, conhecida como "NeonPulse", é uma prodígio da programação espacial. Criada em uma estação orbital, ela domina linguagens de código como se fossem sua segunda língua. Especialista em inteligência artificial e segurança cibernética.`,
+    imgDesktop: "./src/img/bg-neonpulse.png",
+    imgMobile: "./src/img/bg-neonpulse-mobile.png",
+    imgIcon: "./src/img/icone-neonpulse.png",
+  },
+  {
+    id: "codebreaker",
+    title: "Codebreaker",
+    description: `No coração de uma estação espacial abandonada, Orion Vex, um ex-engenheiro cibernético, busca desvendar um código perdido há séculos. Dizem que esse código guarda o segredo para uma tecnologia ancestral capaz de redefinir a inteligência artificial.`,
+    imgDesktop: "./src/img/bg-codebreaker.png",
+    imgMobile: "./src/img/bg-codebreaker-mobile.png",
+    imgIcon: "./src/img/icone-codebreaker.png",
+  },
+];
 
-// Passo 1 - Pegar os botões e  no JS pra poder verificar quando o usuário clicar em cima de um deles
-const botoes = document.querySelectorAll(".botao");
-
-// Passo 1 do OBJETIVO 2 - Pegar os personagens no JS pra poder mostrar ou esconder eles
-const personagens = document.querySelectorAll(".personagem");
-
-// Passo 2 - Adicionar a classe "selecionado" no botão que o usuário clicou
-botoes.forEach((botao, indice) => {
-  botao.addEventListener("click", () => {
-
-    // Passo 3 - Verificar se já existe um botão selecionado, se sim, remover a seleção dele
-    const botaoSelecionado = document.querySelector(".botao.selecionado");
-    if (botaoSelecionado) {
-      botaoSelecionado.classList.remove("selecionado");
-    }
-
-    // Agora adicionamos a classe "selecionado" no botão clicado
-    botao.classList.add("selecionado");
-
-    // OBJETIVO 2 - Mostrar as informações do personagem correspondente ao botão clicado
-
-    // Passo 2 - Adicionar a classe "selecionado" no personagem que o usuário selecionou
-    const personagemSelecionado = document.querySelector(".personagem.selecionado");
-    
-    // Passo 3 - Verificar se já existe um personagem selecionado, se sim, remover a seleção dele
-    if (personagemSelecionado) {
-      personagemSelecionado.classList.remove("selecionado");
-    }
-
-    // Agora mostramos o personagem com o mesmo índice do botão clicado
-    personagens[indice].classList.add("selecionado");
-  });
+//Código dentro da função abaixo executa quando a página carregar
+document.addEventListener("DOMContentLoaded", function () {
+  generateDBZ();
 });
+
+//Cria todo o frontend + eventos dos personagens DBZ
+function generateDBZ() {
+  charactersDBZ.forEach((character, index) => {
+    generateDBZButton(character, index);
+
+    //caso seja o primeiro personagem, cria o painel;
+    if (index === 0) generateDBZPanel(character);
+  });
+
+  generateDBZEventListener();
+}
+
+//Cria os events de click dos botões do menu
+function generateDBZEventListener() {
+
+  //Adicinando eventListener nos botões do menu
+  const botoes = document.querySelectorAll(".botao");
+
+  botoes.forEach(function(botao) {
+    botao.addEventListener('click', function(evt) {
+      // Remove a classe 'selecionado' de todos os botões
+      botoes.forEach(b => b.classList.remove('selecionado'));
+
+      // Adiciona o selecionado ao botão clicado
+      const botaoAtual = evt.currentTarget;
+      botaoAtual.classList.add('selecionado');
+
+      //Pega o id do character no botão (param data-character-id)
+      const characterId = botaoAtual.dataset.characterId;
+      const character = charactersDBZ.find((row) => row.id === characterId);
+
+      // Cria o painel DBZ do personagem selecionado
+      generateDBZPanel(character);
+    })
+  })
+}
+
+/**
+ * Cria o HTML do botão DBZ
+ * @param object character (object of array charactersDBZ) 
+ * @param index (ordem do personagem no array, se for 0 será o primeiro)
+ */
+function generateDBZButton(character, index) {
+  const panel = document.querySelector('.botoes');
+  const html = `
+    <li id="${character.id}_button">
+      <button 
+        data-character-id="${character.id}" 
+        class="botao ${character.id} ${index === 0 ? 'selecionado' : ''}"
+      >
+        <img 
+          src="${character.imgIcon}" 
+          alt="${character.title}" 
+          title="${character.title}"
+        >
+      </button>
+    </li>
+  `;
+
+  panel.insertAdjacentHTML("beforeend", html);
+}
+
+/**
+ * Substitui o painel para o personagem selecionado no momento
+ * @param object character (object of array charactersDBZ)
+ */
+function generateDBZPanel(character) {
+  const panel = document.querySelector('.personagens');
+  const html = `
+    <div class="personagem selecionado">
+      <picture>
+        <source 
+          srcset="${character.imgDesktop}" 
+          media="(min-width: 768px)"
+        >
+        <img 
+          class="imagem" 
+          src="${character.imgMobile}" 
+          alt="Imagem ${character.id}"
+        >
+      </picture>
+      <div class="conteudo">
+        <h2 class="nome-personagem">${character.title}</h2>
+        <p class="descricao">${character.description}</p>
+      </div>
+    </div>
+  `;
+
+  panel.innerHTML = html;
+}
